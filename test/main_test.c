6c4f0a4119d4b188ee3535e0256dbef7eaa9d82d
Jonathan <jonathanelian64@gmail.com>
2024-11-05 22:07:42 -0600

    feat: update tests

--- test/main_test.c
+++ test/main_test.c
@@ -2,8 +2,7 @@
 #include <CUnit/Basic.h>
 #include "../lib/hex.h"
 
-void test_val_hxchar(void)
-{
+void test_val_hxchar(void) {
 	CU_ASSERT(val_hxchar('a') == 10);
 	CU_ASSERT(val_hxchar('f') == 15);
 	CU_ASSERT(val_hxchar('4') == 4);
@@ -12,6 +11,14 @@ void test_val_hxchar(void)
 void test_from_hex(void) {
 	CU_ASSERT(from_hex("0x40") == 64);
 	CU_ASSERT(from_hex("0xa2") == 162);
+	CU_ASSERT(from_hex("XYZ") == 0);
+}
+
+void test_upper() {
+	CU_ASSERT(upper('a') == 'A');
+	CU_ASSERT(upper('l') == 'L');
+	CU_ASSERT(upper('b') == 'B');
+
 }
 
 int init_s() {
@@ -30,13 +37,21 @@ int main() {
 		return CU_get_error();
 	
 	pSuite = CU_add_suite("Suite_1", init_s, clean_s);
-	CU_add_test(pSuite, "Validation value of char hex", test_val_hxchar);
-	CU_add_test(pSuite, "From string hex", test_from_hex);
+	if (NULL == CU_add_test(pSuite, "Validation value of char hex", test_val_hxchar) ||
+	   (NULL == CU_add_test(pSuite, "From string hex", test_from_hex))||
+	   (NULL == CU_add_test(pSuite, "Upper fn", test_upper))) {
+		CU_cleanup_registry();
+		return CU_get_error();
+
+	}
+
 	if (NULL == pSuite) {
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 	
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_show_failures(CU_get_failure_list());
 	CU_basic_run_tests();
 	CU_cleanup_registry();
 	return 0;
