b2d314c1bca5e343452210d22e4ad743783d39bf
Jonathan <jonathanelian64@gmail.com>
2025-07-11 13:19:45 -0600

    feat: usage circular list

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
-#include "../lib/hex.h"
+#include "../lib/circular.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char* argv[]) {
-	int value = 10;
-	char* name = "Juan";
-	hex_app();
+int main(int argc, char *argv[]) {
+	list_t* cl = new_list();
+	list_insert_node(cl, 55);
+	list_insert_node(cl, 24);
+	list_insert_node(cl, 67);
+	list_insert_node(cl, 101);
+	list_log(cl); 
+	list_delete_node(cl, 67);
+	list_log(cl); 
+	list_search(cl, 77);
+	list_insert_node(cl, 88);
+	list_log(cl); 
+	list_search(cl, 101);
+	int* items = list_get_items(cl);
+	for (int i = 0; i < cl->count; i++) {
+		printf("%d ", items[i]);
+	}
+	printf("\n");
+
+	del_list(cl);
+	free(items);
+	
 	return 0;
 }
-
