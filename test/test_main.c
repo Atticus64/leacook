250b7f8c4af780d6430ac57a8b35ca1233fdf6cc
Jonathan <jonathanelian64@gmail.com>
2026-04-07 19:57:53 -0700

    test: contador de failures correcto en error code

--- test/test_main.c
+++ test/test_main.c
@@ -16,31 +16,32 @@ int main(void) {
 
     CU_pSuite arr = CU_add_suite("Array Suite", NULL, NULL);
     if (!arr) goto cleanup;
-    CU_add_test(arr, "test_new_array_valid",     test_new_array_valid);
-    CU_add_test(arr, "test_new_array_invalid",   test_new_array_invalid);
-    CU_add_test(arr, "test_array_append_single", test_array_append_single);
+    CU_add_test(arr, "test_new_array_valid",       test_new_array_valid);
+    CU_add_test(arr, "test_new_array_invalid",     test_new_array_invalid);
+    CU_add_test(arr, "test_array_append_single",   test_array_append_single);
     CU_add_test(arr, "test_array_append_multiple", test_array_append_multiple);
-    CU_add_test(arr, "test_array_append_resize", test_array_append_resize);
-    CU_add_test(arr, "test_array_count_tracking", test_array_count_tracking);
-    CU_add_test(arr, "test_array_no_leak",       test_array_no_leak);
+    CU_add_test(arr, "test_array_append_resize",   test_array_append_resize);
+    CU_add_test(arr, "test_array_count_tracking",  test_array_count_tracking);
+    CU_add_test(arr, "test_array_no_leak",         test_array_no_leak);
 
     CU_pSuite tree = CU_add_suite("Tree Suite", NULL, NULL);
     if (!tree) goto cleanup;
-    CU_add_test(tree, "test_tree_new",           test_tree_new);
-    CU_add_test(tree, "test_tree_add_single",    test_tree_add_single);
-    CU_add_test(tree, "test_tree_add_multiple",  test_tree_add_multiple);
-    CU_add_test(tree, "test_tree_add_duplicate", test_tree_add_duplicate);
-    CU_add_test(tree, "test_tree_items_count",   test_tree_items_count);
-    CU_add_test(tree, "test_tree_inorder_sorted",test_tree_inorder_sorted);
-    CU_add_test(tree, "test_tree_preorder",      test_tree_preorder);
-    CU_add_test(tree, "test_tree_delete",        test_tree_delete);
-    CU_add_test(tree, "test_tree_no_leak",       test_tree_no_leak);
+    CU_add_test(tree, "test_tree_new",            test_tree_new);
+    CU_add_test(tree, "test_tree_add_single",     test_tree_add_single);
+    CU_add_test(tree, "test_tree_add_multiple",   test_tree_add_multiple);
+    CU_add_test(tree, "test_tree_add_duplicate",  test_tree_add_duplicate);
+    CU_add_test(tree, "test_tree_items_count",    test_tree_items_count);
+    CU_add_test(tree, "test_tree_inorder_sorted", test_tree_inorder_sorted);
+    CU_add_test(tree, "test_tree_preorder",       test_tree_preorder);
+    CU_add_test(tree, "test_tree_delete",         test_tree_delete);
+    CU_add_test(tree, "test_tree_no_leak",        test_tree_no_leak);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
 
-cleanup:
+cleanup:;
+    int failures = CU_get_number_of_failures();
     CU_cleanup_registry();
-    return CU_get_error();
+    return failures;
 }
 
